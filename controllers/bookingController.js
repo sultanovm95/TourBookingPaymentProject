37a7a17866e78d93b200a22a08f31e1d2417e526
Muhammadorif Sultanov <sultanovm95@gmail.com>
2021-08-17 18:06:53 -0400

    Updated session amount path

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -44,9 +44,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // Create Booking Function
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = User.findOne({ email: session.customer_email }).id;
-  const price = session.line_items[0].amount / 100;
-  console.log(tour);
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
