a925913d73683412196afa093a002749c20ba6d7
Muhammadorif Sultanov <sultanovm95@gmail.com>
2021-08-17 17:38:11 -0400

    Updated async error

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -40,7 +40,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 // Create Booking Function
-const createBookingCheckout = (session) => {
+const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = User.findOne({ email: session.customer_email }).id;
   const price = session.line_items[0].amount / 100;
