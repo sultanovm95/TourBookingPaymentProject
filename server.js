83cf9b3fc9a306fe874531b5e45bdcd06907bb04
Muhammadorif Sultanov <sultanovm95@gmail.com>
2021-08-17 00:39:25 -0400

    Updated heroku config

--- server.js
+++ server.js
@@ -56,3 +56,10 @@ process.on('uncaughtException', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully!');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
